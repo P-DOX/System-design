81c77786c8577c8e9972c202069d3370d97b71a5
Gaurav Kumar <gaurav0458@gmail.com>
2026-07-10 00:01:14 +0530

    Facade design pattern

---
+++ LLD/Structural_Facade.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TV {
+    public:
+        void display(bool power){
+            cout<<"TV is "<<(power ? "ON" : "OFF")<<endl;
+        }    
+};
+
+class Sound {
+    public:
+        void SetVolume(int vol) {
+            cout<<"Volume is "<<vol<<endl;
+        }
+};
+
+class Brightness {
+    public:
+        void SetBrightness(int val) {
+            cout<<"Brightness is "<<val<<endl;
+        }
+};
+
+class HomeTheatre {
+    TV* tv_;
+    Sound* sound_;
+    Brightness* brightness_;
+
+    public:
+        HomeTheatre() {
+            tv_ = new TV();
+            sound_ = new Sound();
+            brightness_ = new Brightness();
+        }
+
+        void Start() {
+            tv_->display(true);
+            sound_->SetVolume(47);
+            brightness_->SetBrightness(79);
+        }
+
+        void Stop() {
+            tv_->display(false);
+            sound_->SetVolume(0);
+            brightness_->SetBrightness(0);
+        }
+};
+
+int main() {
+    HomeTheatre* ht = new HomeTheatre();
+    ht->Start();
+    ht->Stop();
+}
